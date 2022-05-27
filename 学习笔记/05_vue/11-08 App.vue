c7ef7bc5a5c81eba71b361be70a02872511be79f
liarboss <124275630@qq.com>
2022-05-27 14:09:16 +0800

    update

---
+++ 学习笔记/05_vue/11-08 App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <School></School>
+        <Student></Student>
+    </div>
+</template>
+
+<script>
+import Student from './11-08 Student.vue'
+import School from './11-08 School2.vue'
+
+export default {
+    name: 'App',
+    components: {
+        School,
+        Student
+    }
+}
+</script>
---
+++ 学习笔记/05_vue/11-08 main.js
@@ -0,0 +1,7 @@
+import App from './11-08 App.vue'
+
+new Vue({
+    template:`<App></App>`,
+    el:'#root',
+    components:{App}
+})
