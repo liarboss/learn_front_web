a8dfe049866c7f8545187a8db911ca7784b8ff82
liarboss <124275630@qq.com>
2022-06-02 10:58:23 +0800

    update

---
+++ 学习笔记/05_vue/22_router_param/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col-xs-offset-2 col-xs-8">
+        <div class="page-header">
+          <h2>Vue Router Demo</h2>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-2 col-xs-offset-2">
+        <div class="list-group">
+          <!-- 原始html中我们使用a标签实现页面的跳转 -->
+          <!-- <a class="list-group-item active" href="./about.html">About</a> -->
+          <!-- <a class="list-group-item" href="./home.html">Home</a> -->
+
+          <!-- Vue中借助router-link标签实现路由的切换 -->
+          <!-- <router-link class="list-group-item" active-class="active" to="/about">About</router-link>
+          <router-link class="list-group-item" active-class="active" to="/home">Home</router-link> -->
+
+          <!-- 跳转并携带params参数，to的字符串写法 -->
+          <!-- <router-link :to="/home/message/detail/666/你好">跳转</router-link> -->
+
+          <!-- 跳转并携带params参数，to的对象写法 -->
+          <router-link :to="{
+            name: 'xiangqing',
+            params: {
+              id: 666,
+              title: '你好'
+            }
+          }">跳转</router-link>
+        </div>
+      </div>
+      <div class="col-xs-6">
+        <div class="panel">
+          <div class="panel-body">
+            <!-- 指定组件的呈现位置 -->
+            <router-view></router-view>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
